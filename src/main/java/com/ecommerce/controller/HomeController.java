1b306d53bd9c02c14ffbc6edb155f18e6bb5d793
Gabriel Calle <111298148+GatoNegro5@users.noreply.github.com>
2025-02-25 18:24:18 -0500

    mostrar la vista del producto al usuario

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -2,10 +2,13 @@ package com.ecommerce.controller;
 
 import javax.management.AttributeValueExp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.service.ProductoService;
@@ -14,6 +17,10 @@ import com.ecommerce.service.ProductoService;
 //Cuando vaya a la Raiz del Proyecto, este controlador me va a Mapear los metodos que esten aqui  
 @RequestMapping("/")   
 public class HomeController {
+	
+	//Creamos LOGGER para ver en Console la informacion tambien y verificarla
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
+	
 	
 	//Variable q me permita obtener los Productos. Autowired inyecte una instacia de Clase en el contenedor del framework
 	@Autowired
@@ -24,5 +31,11 @@ public class HomeController {
 		
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("Id Producto enviado como parametro {}",id);
+		return "usuario/productoHome";
 	}
 }
