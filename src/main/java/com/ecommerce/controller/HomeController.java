c9fc458aae804fa065c2757a83f8c8e70b5510b3
Gabriel Calle <111298148+GatoNegro5@users.noreply.github.com>
2025-03-04 19:04:43 -0500

    mostrar informacion del producto en la Pag ProductoHome

--- src/main/java/com/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.ecommerce.controller;
 
+import java.util.Optional;
+
 import javax.management.AttributeValueExp;
 
 import org.slf4j.Logger;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecommerce.model.Producto;
 import com.ecommerce.service.ProductoService;
 
 @Controller
@@ -34,8 +37,14 @@ public class HomeController {
 	}
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {   //Model lleva inf a la Vista
 		log.info("Id Producto enviado como parametro {}",id);
+		
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);   //Me trae una lista de tipo Optional
+		producto = productoOptional.get();		
+		model.addAttribute("producto", producto);
+		
 		return "usuario/productoHome";
 	}
 }
